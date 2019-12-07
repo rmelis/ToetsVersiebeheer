1bc3d0f6f111339d665c3fdacf442409dbdd43d0
rmelis <57659595+rmelis@users.noreply.github.com>
2019-12-07 19:57:20 +0100

    Add files via upload

---
+++ ArrayPrinter.java
@@ -0,0 +1,13 @@
+package ToetsVersiebeheer;
+
+public class ArrayPrinter {
+	public static void main(String[] args) {
+		int[] array = {10, 20, 30, 40};
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+	public void printarray(int[] array) {
+		
+	}
+}
